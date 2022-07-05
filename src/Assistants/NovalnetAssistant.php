1f9dc9f41ac0c407de6f4f84032d83974e7b7d9f
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2022-07-05 18:00:46 +0530

    Update NovalnetAssistant.php

--- src/Assistants/NovalnetAssistant.php
+++ src/Assistants/NovalnetAssistant.php
@@ -18,6 +18,7 @@ namespace Novalnet\Assistants;
 use Plenty\Modules\Wizard\Services\WizardProvider;
 use Novalnet\Assistants\SettingsHandlers\NovalnetAssistantSettingsHandler;
 use Plenty\Modules\System\Contracts\WebstoreRepositoryContract;
+use Plenty\Plugin\Application;
 use Plenty\Plugin\Log\Loggable;
 
 /**
