52ccf2ee35e549489d7768874794db6ed2d1faad
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2022-07-22 15:45:08 +0530

    Add files via upload

---
+++ src/Services/SettingsService.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Novalnet\Services;
+
+use Carbon\Carbon;
+use Novalnet\Models\Settings;
+use Plenty\Modules\Plugin\DataBase\Contracts\DataBase;
+use Plenty\Modules\Plugin\PluginSet\Contracts\PluginSetRepositoryContract;
+use Plenty\Plugin\Application;
+
+/**
+ * Class SettingsService
+ *
+ * @package Novalnet\Services\SettingsService
+ */
+class SettingsService
+{ 
+	/**
+     * @var DataBase
+     */
+    protected $database;
+    
+    /**
+     * SettingsService constructor.
+     * @param DataBase $database
+     * @param CachingRepository $cachingRepository
+     */
+    public function __construct(DataBase $database)
+    {
+        $this->database = $database;
+    }
+    
+	public function getSettings($clientId, $pluginSetId)
+	{
+		if (is_null($clientId)) {
+            /** @var Application $application */
+            $application = pluginApp(Application::class);
+            $clientId = $application->getPlentyId();
+        }
+        
+        if (is_null($pluginSetId)) {
+            /** @var PluginSetRepositoryContract $pluginSetRepositoryContract */
+            $pluginSetRepositoryContract = pluginApp(PluginSetRepositoryContract::class);
+            $pluginSetId = $pluginSetRepositoryContract->getCurrentPluginSetId();
+        }
+        
+        /** @var Settings[] $setting */
+        $setting = $this->database->query(Settings::class)->where('clientId', '=', $clientId)
+													      ->where('pluginSetId', '=', $pluginSetId)
+														  ->limit(1)
+														  ->get();
+	}
+	
+	public function createOrUpdateNovalnetConfigurationSettings($clientId, $pluginSetId)
+	{
+		
+		
+	}
+}
+
