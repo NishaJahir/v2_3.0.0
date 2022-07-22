0cd3d2ecdd9e5a9c2c3ab128c083c0d3eac4713f
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2022-07-22 15:49:37 +0530

    Update SettingsService.php

--- src/Services/SettingsService.php
+++ src/Services/SettingsService.php
@@ -16,48 +16,49 @@ use Plenty\Plugin\Log\Loggable;
  */
 class SettingsService
 { 
-    use Loggable;
-     
-    /**
-    * @var DataBase
-    */
-    protected $database;
-    
-    /**
-     * SettingsService constructor.
-     * @param DataBase $database
-     * @param CachingRepository $cachingRepository
-     */
-    public function __construct(DataBase $database)
-    {
-        $this->database = $database;
-    }
+	    use Loggable;
+
+       /**
+       * @var DataBase
+       */
+       protected $database;
+
+       /**
+       * SettingsService constructor.
+       * @param DataBase $database
+       * @param CachingRepository $cachingRepository
+       */
+	public function __construct(DataBase $database)
+	{
+	    $this->database = $database;
+	}
     
 	public function getSettings($clientId, $pluginSetId)
 	{
 		if (is_null($clientId)) {
-            /** @var Application $application */
-            $application = pluginApp(Application::class);
-            $clientId = $application->getPlentyId();
-        }
-        
-        if (is_null($pluginSetId)) {
-            /** @var PluginSetRepositoryContract $pluginSetRepositoryContract */
-            $pluginSetRepositoryContract = pluginApp(PluginSetRepositoryContract::class);
-            $pluginSetId = $pluginSetRepositoryContract->getCurrentPluginSetId();
-        }
-        
-        /** @var Settings[] $setting */
-        $setting = $this->database->query(Settings::class)->where('clientId', '=', $clientId)
-													      ->where('pluginSetId', '=', $pluginSetId)
-														  ->limit(1)
-														  ->get();
+	    /** @var Application $application */
+	    $application = pluginApp(Application::class);
+	    $clientId = $application->getPlentyId();
 	}
-	
+
+	if (is_null($pluginSetId)) {
+	    /** @var PluginSetRepositoryContract $pluginSetRepositoryContract */
+	    $pluginSetRepositoryContract = pluginApp(PluginSetRepositoryContract::class);
+	    $pluginSetId = $pluginSetRepositoryContract->getCurrentPluginSetId();
+	}
+
+	/** @var Settings[] $setting */
+	$setting = $this->database->query(Settings::class)->where('clientId', '=', $clientId)
+							  ->where('pluginSetId', '=', $pluginSetId)
+							  ->get();
+		
+		$this->getLogger(__METHOD__)->error('db', $setting);
+	}
+
 	public function createOrUpdateNovalnetConfigurationSettings($clientId, $pluginSetId)
 	{
-		
-		
+
+
 	}
 }
 
