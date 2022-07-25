d819e3e3ec6a2f4a26c3accaf7d7ca6738ae3334
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2022-07-26 01:36:52 +0530

    Update NovalnetServiceProvider.php

--- src/Providers/NovalnetServiceProvider.php
+++ src/Providers/NovalnetServiceProvider.php
@@ -19,11 +19,15 @@ use Plenty\Plugin\ServiceProvider;
 use Plenty\Modules\Basket\Events\Basket\AfterBasketCreate;
 use Plenty\Modules\Basket\Events\Basket\AfterBasketChanged;
 use Plenty\Modules\Basket\Events\BasketItem\AfterBasketItemAdd;
+use Plenty\Plugin\Events\Dispatcher;
+use Plenty\Modules\Payment\Events\Checkout\ExecutePayment;
+use Plenty\Modules\Payment\Events\Checkout\GetPaymentMethodContent;
 use Plenty\Modules\Payment\Method\Contracts\PaymentMethodContainer;
 use Novalnet\Helper\PaymentHelper;
 use Plenty\Modules\Wizard\Contracts\WizardContainerContract;
 use Novalnet\Assistants\NovalnetAssistant;
 use Novalnet\Methods\NovalnetPaymentAbstract;
+use Plenty\Modules\Frontend\Session\Storage\Contracts\FrontendSessionStorageFactoryContract;
 use Plenty\Plugin\Log\Loggable;
 
 /**
@@ -45,13 +49,23 @@ class NovalnetServiceProvider extends ServiceProvider
 
     /**
      * Boot additional services for the payment method
-     *
-     * @param PaymentMethodContainer $payContainer
      * 
+     * @param Dispatcher $eventDispatcher
+     * @param BasketRepositoryContract $basket
+     * @param PaymentMethodContainer $payContainer
      */
-    public function boot(PaymentMethodContainer $payContainer)
+    public function boot(Dispatcher $eventDispatcher,
+                        BasketRepositoryContract $basketRepository,
+                        PaymentMethodContainer $payContainer,
+                        PaymentHelper $paymentHelper, 
+                        FrontendSessionStorageFactoryContract $sessionStorage
+                        )
     {
         $this->registerPaymentMethods($payContainer);
+        
+        $this->registerPaymentRendering($eventDispatcher, $basketRepository);
+
+        $this->registerPaymentExecute($eventDispatcher, $basketRepository);
         
         pluginApp(WizardContainerContract::class)->register('payment-novalnet-assistant', NovalnetAssistant::class);
     }
@@ -71,5 +85,78 @@ class NovalnetServiceProvider extends ServiceProvider
                 AfterBasketCreate::class
             ]);
         }
+    }
+    
+    
+    /**
+     * Rendering the Novalnet payment method content
+     *
+     * @param Dispatcher $eventDispatcher
+     * @param BasketRepositoryContract $basketRepository
+     * @param PaymentHelper $paymentHelper
+     * @param PaymentService $paymentService
+     */
+    protected function registerPaymentRendering(Dispatcher $eventDispatcher,
+                                              BasketRepositoryContract $basketRepository,
+                                              PaymentHelper $paymentHelper,
+                                              PaymentService $paymentService
+                                              )
+    {
+        // Listen for the event that gets the payment method content
+        $eventDispatcher->listen(
+            GetPaymentMethodContent::class, 
+            function(GetPaymentMethodContent $event) use($basketRepository, $paymentHelper, $sessionStorage) {
+				
+			if($paymentHelper->getPaymentKeyByMop($event->getMop())) {
+				$paymentKey = $paymentHelper->getPaymentKeyByMop($event->getMop());
+				if(in_array($paymentKey, ['NOVALNET_INVOICE'])) {
+					 $content = '';
+                     $contentType = 'continue';
+                     $paymentRequestData = $paymentService->getRequestParameters($basketRepository->load(), $paymentKey);
+					 if(empty($paymentRequestData['customer']['first_name']) && empty($paymentRequestData['customer']['last_name'])) {
+							$content = $paymentHelper->getTranslatedText('nn_first_last_name_error');
+							$contentType = 'errorCode';   
+                     }
+                     $sessionStorage->getPlugin()->setValue('nnPaymentData', $paymentRequestData);
+				}
+				
+				$event->setValue($content);
+				$event->setType($contentType);
+			}
+			
+                        
+        });
+    
+    }
+    
+     /**
+     * Execute the Novalnet payment method
+     *
+     * @param Dispatcher $eventDispatcher
+     * @param PaymentHelper $paymentHelper
+     * @param PaymentService $paymentService
+     * @param FrontendSessionStorageFactoryContract $sessionStorage
+     */
+    protected function registerPaymentExecute(Dispatcher $eventDispatcher,
+                                               PaymentHelper $paymentHelper,
+                                               PaymentService $paymentService,
+                                               FrontendSessionStorageFactoryContract $sessionStorage
+                                              )
+    {
+        // Listen for the event that executes the payment
+        $eventDispatcher->listen(
+			ExecutePayment::class,
+            function (ExecutePayment $event) use ($paymentHelper, $paymentService, $sessionStorage)
+            {
+                if($paymentHelper->getPaymentKeyByMop($event->getMop())) {
+                    $sessionStorage->getPlugin()->setValue('nnOrderNo',$event->getOrderId());
+                    $sessionStorage->getPlugin()->setValue('mop',$event->getMop());
+                    $paymentKey = $paymentHelper->getPaymentKeyByMop($event->getMop());
+                    $sessionStorage->getPlugin()->setValue('paymentkey', $paymentKey);
+                    $paymentService->performServerCall();
+                    $paymentService->validateResponse();
+                }
+            });
+    
     }
 }
