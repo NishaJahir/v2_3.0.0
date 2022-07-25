743f936b5db89d44d49d722115ee2b10af674eff
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2022-07-25 18:31:10 +0530

    Add files via upload

---
+++ src/Constants/NovalnetConstants.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * This module is used for real time processing of
+ * Novalnet payment module of customers.
+ * This free contribution made by request.
+ * 
+ * If you have found this script useful a small
+ * recommendation as well as a comment on merchant form
+ * would be greatly appreciated.
+ *
+ * @author       Novalnet AG
+ * @copyright(C) Novalnet 
+ * All rights reserved. https://www.novalnet.de/payment-plugins/kostenlos/lizenz
+ */
+ 
+namespace Novalnet\Constants;
+
+/**
+ * Class NovalnetConstants
+ *
+ * @package Novalnet\Constants
+ */
+class NovalnetConstants
+{
+    const PLUGIN_VERSION = '7.0.0-NN(3.0.0)';
+}
