e5682d898f62e8a0d52db48156d6394d3cfedafb
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2022-08-10 15:52:15 +0530

    Update PaymentService.php

--- src/Services/PaymentService.php
+++ src/Services/PaymentService.php
@@ -374,7 +374,6 @@ class PaymentService
                 // Transaction secret used for the later checksum verification
                 $txnSecret = $this->sessionStorage->getPlugin()->setValue('txnSecret', $paymentResponseData['transaction']['txn_secret']);
                 header('Location: ' . $paymentResponseData['result']['redirect_url']);
-                exit;
             } else {
                 $this->pushNotification($paymentResponseData['result']['status_text'], 'error', 100);
             }
