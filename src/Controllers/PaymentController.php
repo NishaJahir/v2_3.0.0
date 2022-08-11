858dc2431f1cb2f0146acd6e327df182632481f7
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2022-08-11 15:07:00 +0530

    Update PaymentController.php

--- src/Controllers/PaymentController.php
+++ src/Controllers/PaymentController.php
@@ -21,7 +21,6 @@ use Plenty\Plugin\Http\Response;
 use Novalnet\Services\PaymentService;
 use Novalnet\Services\SettingsService;
 use Plenty\Modules\Frontend\Session\Storage\Contracts\FrontendSessionStorageFactoryContract;
-use Novalnet\Constants\NovalnetConstants;
 use Plenty\Plugin\Log\Loggable;
 
 /**
@@ -115,42 +114,5 @@ class PaymentController extends Controller
         }
        
         return $this->response->redirectTo($this->sessionStorage->getLocaleSettings()->language . '/confirmation');
-    }
-    
-    /**
-     * Validate the checksum generated for redirection payments
-     *
-     * @param  array  $paymentResponseData
-     * 
-     * @return array
-     */
-    public function validateChecksumAndGetTxnStatus($paymentResponseData)
-    {
-        if ($paymentResponseData['status'] && $paymentResponseData['status'] == 'SUCCESS') {
-            
-            $nnTxnSecret = $this->sessionStorage->getPlugin()->getValue('nnTxnSecret');
-            $this->getLogger(__METHOD__)->error('secret value', $nnTxnSecret);
-            $strRevPrivateKey = $this->paymentHelper->reverseString($this->settingsService->getNnPaymentSettingsValue('novalnet_private_key'));
-           
-            // Condition to check whether the payment is redirect
-            if (!empty($paymentResponseData['checksum']) && !empty($paymentResponseData['tid']) && !empty($nnTxnSecret)) {                            
-                $generatedChecksum = hash('sha256', $paymentResponseData['tid'] . $nnTxnSecret . $paymentResponseData['status'] . $strRevPrivateKey);
-                // If the checksum isn't matching, there could be a possible manipulation in the data received 
-                if ($generatedChecksum !== $paymentResponseData['checksum']) {
-                    $checksumInvalidMsg = $this->paymentHelper->getTranslatedText('checksum_error');                                  
-                    $this->paymentService->pushNotification($checksumInvalidMsg, 'error', 100);
-                    return $this->response->redirectTo($this->sessionStorage->getLocaleSettings()->language . '/confirmation');
-                }
-            }
-                                          
-            $paymentRequestData = [];
-            $paymentRequestData['transaction']['tid'] = $paymentResponseData['tid'];
-            
-            $privatekey = $this->settingsService->getNnPaymentSettingsValue('novalnet_private_key');
-            return $this->paymentHelper->executeCurl($paymentRequestData, NovalnetConstants::TXN_RESPONSE_URL, $privatekey);
-        } else {
-            $this->paymentService->pushNotification($paymentResponseData['status_text'], 'error', 100);
-            return $this->response->redirectTo($this->sessionStorage->getLocaleSettings()->language . '/confirmation');
-        }                  
     }
 }
