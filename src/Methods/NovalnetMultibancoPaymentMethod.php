2a88d5841154d24b0360d1876dd2a8ebab14034e
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2022-08-25 11:34:03 +0530

    Add files via upload

---
+++ src/Methods/NovalnetMultibancoPaymentMethod.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * This module is used for real time processing of
+ * Novalnet payment module of customers.
+ * This free contribution made by request.
+ * 
+ * If you have found this script useful a small
+ * recommendation as well as a comment on merchant form
+ * would be greatly appreciated.
+ *
+ * @author       Novalnet AG
+ * @copyright(C) Novalnet
+ * All rights reserved. https://www.novalnet.de/payment-plugins/kostenlos/lizenz
+ */
+
+namespace Novalnet\Methods;
+
+
+/**
+ * Class NovalnetMultibancoPaymentMethod
+ *
+ * @package Novalnet\Methods
+ */
+class NovalnetMultibancoPaymentMethod extends NovalnetPaymentAbstract
+{
+    const PAYMENT_KEY = 'NOVALNET_MULTIBANCO';
+}
---
+++ src/Methods/NovalnetWechatPayPaymentMethod.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * This module is used for real time processing of
+ * Novalnet payment module of customers.
+ * This free contribution made by request.
+ * 
+ * If you have found this script useful a small
+ * recommendation as well as a comment on merchant form
+ * would be greatly appreciated.
+ *
+ * @author       Novalnet AG
+ * @copyright(C) Novalnet
+ * All rights reserved. https://www.novalnet.de/payment-plugins/kostenlos/lizenz
+ */
+
+namespace Novalnet\Methods;
+
+
+/**
+ * Class NovalnetWechatPayPaymentMethod
+ *
+ * @package Novalnet\Methods
+ */
+class NovalnetWechatPayPaymentMethod extends NovalnetPaymentAbstract
+{
+    const PAYMENT_KEY = 'NOVALNET_WECHAT_PAY';
+}
