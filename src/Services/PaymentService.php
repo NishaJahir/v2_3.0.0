e798b060dbdafe9cd7ad9d2523cc0f4912027e60
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2022-09-03 15:40:47 +0530

    Update PaymentService.php

--- src/Services/PaymentService.php
+++ src/Services/PaymentService.php
@@ -556,36 +556,72 @@ class PaymentService
         return json_encode($additionalInfo);
     }
     
-    public function isGuaranteePaymentToBeDisplayed(Basket $basket, $paymentKey)
-    {
+   public function isGuaranteePaymentToBeDisplayed(Basket $basket, $paymentKey)
+   {
         try {
             if(!is_null($basket) && $basket instanceof Basket && !empty($basket->customerInvoiceAddressId)) {
                 // Check if the guaranteed payment method is enabled
                 if($this->settingsService->getNnPaymentSettingsValue('payment_active', $paymentKey) == true) {
-                    
+
                     // Get the customer billing and shipping details
                     if(!empty($basket->customerInvoiceAddressId)) {
-                    $billingAddress = $this->paymentHelper->getCustomerBillingOrShippingAddress((int) $basket->customerInvoiceAddressId);
-                    $shippingAddress = $billingAddress;
+                       $billingAddress = $this->paymentHelper->getCustomerBillingOrShippingAddress((int) $basket->customerInvoiceAddressId);
+                       $shippingAddress = $billingAddress; 
                     }
-
+                    
                     if(!empty($basket->customerShippingAddressId)) {
                         $shippingAddress = $this->paymentHelper->getCustomerBillingOrShippingAddress((int) $basket->customerShippingAddressId);
                     }
-$this->getLogger(__METHOD__)->error('cal S', $shippingAddress);
+
                     // Get the billing and shipping details
                     $billingShippingDetails = $this->paymentHelper->getRequiredBillingShippingDetails($billingAddress, $shippingAddress);
-                    
-$this->getLogger(__METHOD__)->error('billingShippingDetails', $billingShippingDetails);
-                    return 'guarantee';
+
+                    // Set the minimum guaranteed amount
+                    $configuredMinimumGuaranteedAmount = $this->settingsService->getNnPaymentSettingsValue('minimum_guaranteed_amount', $paymentKey);
+
+                    $minimumGuaranteedAmount = !empty($configuredMinimumGuaranteedAmount) ? $configuredMinimumGuaranteedAmount : 999;
+
+                    /** @var \Plenty\Modules\Frontend\Services\VatService $vatService */
+                    $vatService = pluginApp(\Plenty\Modules\Frontend\Services\VatService::class);
+
+                    //we have to manipulate the basket because its stupid and doesnt know if its netto or gross
+                    if(!count($vatService->getCurrentTotalVats())) {
+                        $basket->itemSum = $basket->itemSumNet;
+                        $basket->shippingAmount = $basket->shippingAmountNet;
+                        $basket->basketAmount = $basket->basketAmountNet;
+                    }
+
+                    // First, we check the billing and shipping addresses are matched
+                    // Second, we check the customer from the guaranteed payments supported countries
+                    // Third, we check if the supported currency is selected
+                    // Finally, we check if the minimum order amount configured to process the payment method. By default, the minimum order amount is 999 cents
+                    if( $billingShippingDetails['billing'] === $billingShippingDetails['shipping'] && 
+                        (!in_array($billingShippingDetails['billing']['country_code'], ['AT', 'DE', 'CH']) || ($this->settingsService->getNnPaymentSettingsValue('allow_b2b_customer', $paymentKey) && 
+                        !in_array($billingShippingDetails['billing']['country_code'], $this->getEuropeanRegionCountryCodes()))) && 
+                        $basket->currency == 'EUR' && 
+                        (!empty($minimumGuaranteedAmount) &&  $minimumGuaranteedAmount > $basket->basketAmount)) 
+
+                    {
+                        // If the guaranteed conditions are met, display the guaranteed payments
+                        return 'guarantee';
+                    }
+
+                    // Further we check if the normal payment method can be enabled if the condition not met 
+                    if ($this->settingsService->getNnPaymentSettingsValue('force', $paymentKey) == true) {
+                        return 'normal';
+                    }
+
+                    // If none matches, error message displayed 
+                    return 'error'; 
                 }
-                
+                // If payment guarantee is not enabled, we show default one 
+                return 'normal';
             }
+            // If payment guarantee is not enabled, we show default one 
+            return 'normal';
         } catch(\Exception $e) {
             $this->getLogger(__METHOD__)->error('Novalnet::isGuaranteePaymentToBeDisplayedFailed', $e);
-           
         }
-      
     }
     
     /**
