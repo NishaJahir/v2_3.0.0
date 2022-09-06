69befea64caf65087e0be42ed51c976cbc5caac4
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2022-09-06 20:09:24 +0530

    Update NovalnetOrderConfirmationDataProvider.php

--- src/Providers/NovalnetOrderConfirmationDataProvider.php
+++ src/Providers/NovalnetOrderConfirmationDataProvider.php
@@ -17,6 +17,8 @@ namespace Novalnet\Providers;
 
 use Plenty\Plugin\Templates\Twig;
 use Novalnet\Helper\PaymentHelper;
+use Novalnet\Services\PaymentService;
+use Plenty\Modules\Payment\Contracts\PaymentRepositoryContract;
 
 /**
  * Class NovalnetOrderConfirmationDataProvider
@@ -29,20 +31,26 @@ class NovalnetOrderConfirmationDataProvider
      * Setup the Novalnet transaction comments for the requested order
      *
      * @param Twig $twig
+     * @param PaymentRepositoryContract $paymentRepositoryContract
      * @param Arguments $arg
      * 
      * @return string
      */
-    public function call(Twig $twig, $arg)
+    public function call(Twig $twig, PaymentRepositoryContract $paymentRepositoryContract, $arg)
     {
         $order = $arg[0];
         $paymentHelper = pluginApp(PaymentHelper::class);
+        $paymentService = pluginApp(PaymentService::class);
         if(!empty($order['id'])) {
-            // Check it is Novalnet Payment method order
-            if($paymentHelper->getPaymentKeyByMop($payment->mopId)) {
-                // Get Novalnet transaction details from the Novalnet database table
-                $nnDbTxDetails = $paymentService->getDatabaseValues($orderId);
-                $paymentHelper->logger('nnDbTxDetails', $nnDbTxDetails);
+            // Loads the payments for an order
+            $payments = $paymentRepositoryContract->getPaymentsByOrderId($order['id']);
+            foreach($payments as $payment) {
+                // Check it is Novalnet Payment method order
+                if($paymentHelper->getPaymentKeyByMop($payment->mopId)) {
+                    // Get Novalnet transaction details from the Novalnet database table
+                    $nnDbTxDetails = $paymentService->getDatabaseValues($orderId);
+                    $paymentHelper->logger('nnDbTxDetails', $nnDbTxDetails);
+                }
             }
         }
     }
