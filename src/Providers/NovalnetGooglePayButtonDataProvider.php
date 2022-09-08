9dac883bf006c8e8963fc6a258eba4c2a1e2b58e
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2022-09-08 12:28:47 +0530

    Update NovalnetGooglePayButtonDataProvider.php

--- src/Providers/NovalnetGooglePayButtonDataProvider.php
+++ src/Providers/NovalnetGooglePayButtonDataProvider.php
@@ -57,8 +57,8 @@ class NovalnetGooglePayButtonDataProvider
         $basketitemDetails = $basketItem->all();
         foreach($basketitemDetails as $basketItem) {
             $paymentHelper->logger('basketItem', $basketItem);
-            $paymentHelper->logger('shipping ID', $basketitemDetails[1]->shippingProfileId);
-            $paymentHelper->logger('shipping profile', $shippingProfileRepository->get($basketitemDetails[1]->shippingProfileId));
+            $paymentHelper->logger('shipping ID12', $basketItem->shippingProfileId);
+            $paymentHelper->logger('shipping profile12', $shippingProfileRepository->get($basketItem->shippingProfileId));
         }
         
         
